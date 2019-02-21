211c1588f794e102ae688ceeab52d903a3ca74d8
Sam O'Donnell <samodonnell@Christophers-iMac.local>
2019-02-21 09:20:50 -0500

    Second iteration

---
+++ Gruntfile.js
@@ -0,0 +1,158 @@
+module.exports = function(grunt) {
+
+    var configs = '_js/globals/configs/production.js',
+        i = 0,
+        len = grunt.cli.tasks.length;
+
+    //SET GLOBAL CONFIGS BASED ON ENVIROMENT OR TASK
+    for (i; i < len; i++) {
+        if (grunt.cli.tasks[i] === 'dev') {
+            configs = '_js/globals/configs/development.js';
+        }
+    }
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        critical: {
+            standto: {
+                options: {
+                    inline: true,
+                    minify: true,
+                    base: './',
+                    css: [
+                        '_site/e2/css/rv7/social-media/style.css'
+                    ],
+                    dimensions: [{
+                        height: 768,
+                        width: 1366
+                    }, {
+                        height: 640,
+                        width: 360
+                    }, {
+                        height: 568,
+                        width: 320
+                    }]
+                },
+                src: './_site/index.html',
+                dest: './_site/index.html'
+            },
+            about: {
+                options: {
+                    inline: true,
+                    minify: true,
+                    base: './',
+                    css: [
+                        '_site/e2/css/rv7/social-media/style.css'
+                    ],
+                    dimensions: [{
+                        height: 768,
+                        width: 1366
+                    }, {
+                        height: 640,
+                        width: 360
+                    }, {
+                        height: 568,
+                        width: 320
+                    }]
+                },
+                src: './_site/about/index.html',
+                dest: './_site/about/index.html'
+            },
+            archive: {
+                options: {
+                    inline: true,
+                    minify: true,
+                    base: './',
+                    css: [
+                        '_site/e2/css/rv7/social-media/style.css'
+                    ],
+                    dimensions: [{
+                        height: 768,
+                        width: 1366
+                    }, {
+                        height: 640,
+                        width: 360
+                    }, {
+                        height: 568,
+                        width: 320
+                    }]
+                },
+                src: './_site/archive/index.html',
+                dest: './_site/archive/index.html'
+            }
+        },
+        browserify: {
+            libs: {
+                files: {
+                    '_js/bundled/header.js': ['_js/globals/header.js', '_js/globals/top-btn.js']
+                },
+                options: {
+                    transform: ['babelify']
+                }
+            },
+            social: {
+                files: {
+                    '_js/bundled/stand-to.js': [configs, '_js/stand-to.js', '_js/dinfos-playlist.js', '_js/side-nav.js']
+                },
+                options: {
+                    transform: ['babelify'],
+                    alias: [
+                        './_js/globals/modules/Helper.js:Helper',
+                        './_js/globals/navs.js:Nav',
+                        './node_modules/waypoints/src/waypoint.js:Waypoints'
+                    ]
+                }
+            }
+        },
+        uglify: {
+            options: {
+                mangle: true,
+                compress: true,
+                beautify: false
+            },
+            build: {
+                files: [{
+                    src: [
+                        '_js/bundled/header.js',
+                        '_js/bundled/stand-to.js'
+                    ],
+                    dest: 'e2/js/rv7/stand-to/<%= pkg.name %>.min.js'
+                }]
+            }
+        },
+        usebanner: {
+            dist: {
+                options: {
+                    position: 'top',
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n'
+                },
+                files: {
+                    src: ['_site/e2/js/**/*.js', '_site/e2/css/**/*.css']
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'e2/css/rv7/social-media/style.css': '_scss/_style.scss'
+                }
+            }
+        }
+
+
+    });
+
+    grunt.loadNpmTasks('grunt-critical');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-banner');
+
+    grunt.registerTask('production', ['browserify', 'uglify', 'sass']);
+
+    grunt.registerTask('post-production', ['usebanner', 'critical']);
+
+};
