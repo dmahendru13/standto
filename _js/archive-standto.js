faba99695d79a5d6c8ce6360e10e54bb2bb7b2a1
Sam O'Donnell <samodonnell@Christophers-iMac.local>
2019-03-15 11:31:33 -0400

    Updating 2-15

--- _js/archive-standto.js
+++ _js/archive-standto.js
@@ -1,3 +1,18 @@
-(function () {
-    alert("connected but from annonymous function");
-})();
+// //https://stackoverflow.com/questions/13363567/jquery-javascript-split-a-text-node-into-two-or-three
+// // https://codeburst.io/adding-line-breaks-paragraphs-to-dynamic-text-in-javascript-569cc474c89
+
+
+
+// console.log("connected, great success");
+// (function () {
+//     var content = document.querySelector(".left-column.archived-standto"),
+//         newContent;
+//         //console.log(content);
+
+//     $.each(content, function () {
+//         newContent = $(this).find('what').text();
+//         console.log(newContent);
+//     });
+
+//     //replaceAsterick();
+// })();
