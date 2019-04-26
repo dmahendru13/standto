0ace2ddfab01d7ad5e685419e4c1d8d0f0250cb0
Sam O'Donnell <samodonnell@Christophers-iMac.local>
2019-04-26 08:09:33 -0400

    Restructured ST mock to more closely resemble current file structure.

--- _js/search.js
+++ _js/search.js
@@ -1,35 +1,35 @@
-(function () {
-    if (window.location.search != '') {
-        function getUrlVars() {
-            var vars = {};
-            var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-                vars[key] = value;
-            });
+// (function () {
+//     if (window.location.search != '') {
+//         function getUrlVars() {
+//             var vars = {};
+//             var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+//                 vars[key] = value;
+//             });
 
-            return vars.query;
+//             return vars.query;
 
-        }
+//         }
 
-        getUrlVars();
+//         getUrlVars();
 
-        $(window).load(function () {
-            var i = setInterval(function () {
-                var input = document.querySelector('.ais-search-box--input');
+//         $(window).load(function () {
+//             var i = setInterval(function () {
+//                 var input = document.querySelector('.ais-search-box--input');
 
-                if (input) {
-                    clearInterval(i);
+//                 if (input) {
+//                     clearInterval(i);
 
-                    let unfilteredUrl = getUrlVars();
+//                     let unfilteredUrl = getUrlVars();
 
-                    var filteredUrl = unfilteredUrl.replace(/\+/gi, ' ');
+//                     var filteredUrl = unfilteredUrl.replace(/\+/gi, ' ');
 
-                    let searchBox = document.querySelector('.ais-search-box--input');
+//                     let searchBox = document.querySelector('.ais-search-box--input');
 
-                    searchBox.value = filteredUrl;
+//                     searchBox.value = filteredUrl;
 
-                }
-            }, 100);
-        });
-    }
+//                 }
+//             }, 100);
+//         });
+//     }
 
-})();
+// })();
--- _js/standto.js
+++ _js/standto.js
@@ -1,5 +1,5 @@
 var SocialBar = require('./globals/modules/SocialBar'),
-    //SubNav = require('./globals/modules/SubNav'),
+    SubNav = require('./globals/modules/SubNav'),
     Helper = require('./globals/modules/Helper'),
     //Configs = require('./production'),
     Client = require('node-rest-client').Client;
@@ -26,23 +26,20 @@ var SocialBar = require('./globals/modules/SocialBar'),
         SocialBar.initWaypoint(socialbarwaypoint);
 
         if (subnav) {
-            // if (unsubDiv && hash === '#unsubscribe') {
-            //     Helper.removeClass(unsubDiv, 'hidden');
-            //     Helper.addClass(unsub, 'hidden');
-            //     new SubNav(subnav,
-            //         function() {
-            //             Helper.addClass(unsubDiv, 'hidden');
-            //             setResultText(false, '');
-            //         },
-            //         { initializeEmpty: true }
-            //         );
-            // } else {
-            //     new SubNav(subnav);
-            // }
             if (unsubDiv && hash === '#unsubscribe') {
-                Helper.removeClass(unsubDiv, 'subnav-selected');
+                Helper.removeClass(unsubDiv, 'hidden');
                 Helper.addClass(unsub, 'hidden');
+                new SubNav(subnav,
+                    function() {
+                        Helper.addClass(unsubDiv, 'hidden');
+                        setResultText(false, '');
+                    },
+                    { initializeEmpty: true }
+                    );
+            } else {
+                new SubNav(subnav);
             }
+
         }
 
         //Add special classes to markup headings
@@ -169,101 +166,101 @@ var SocialBar = require('./globals/modules/SocialBar'),
             moveByline();
         }
     } else if (archivesBody) {
-        (function () {
-            // TAKEN FROM ORIGINAL archive.js
-            // Filter out previous Stand-tos older than X number of weeks
-            !function (a) {
-                a.fn.dateFilter = function (b) {
-                    var c = a.extend({ cutoff: new Date, buffer: -35 }, b); c.cutoff.setDate(c.cutoff.getDate() + c.buffer); var d = this.parent(); return this.each(function () { new Date(a(this).data("date-filter")).getTime() < c.cutoff.getTime() && this.remove() }), 0 == d.prop("childElementCount"), this
-                }
-            }(jQuery);
+        // (function () {
+        //     // TAKEN FROM ORIGINAL archive.js
+        //     // Filter out previous Stand-tos older than X number of weeks
+        //     !function (a) {
+        //         a.fn.dateFilter = function (b) {
+        //             var c = a.extend({ cutoff: new Date, buffer: -35 }, b); c.cutoff.setDate(c.cutoff.getDate() + c.buffer); var d = this.parent(); return this.each(function () { new Date(a(this).data("date-filter")).getTime() < c.cutoff.getTime() && this.remove() }), 0 == d.prop("childElementCount"), this
+        //         }
+        //     }(jQuery);
 
-            $('.headlines ul#archive-results li').dateFilter();
+        //     $('.headlines ul#archive-results li').dateFilter();
 
-            $(function () {
-                if ($('.headlines ul#archive-results li').length == -1) {
+        //     $(function () {
+        //         if ($('.headlines ul#archive-results li').length == -1) {
 
-                    var results = document.getElementById('results-text'),
-                        noResults = document.getElementById('no-results-text');
+        //             var results = document.getElementById('results-text'),
+        //                 noResults = document.getElementById('no-results-text');
 
-                    Helper.removeClass(noResults, 'hidden');
-                    Helper.addClass(results, 'hidden');
+        //             Helper.removeClass(noResults, 'hidden');
+        //             Helper.addClass(results, 'hidden');
 
-                }
-            });
+        //         }
+        //     });
 
-        })();
-        (function () {
-            'use strict';
-            var searchForm = document.getElementById('standto_search_form');
-            if (window.location.pathname == '/archives/' || window.location.pathname == '/standto/archives/') {
+        // })();
+        // (function () {
+        //     'use strict';
+        //     var searchForm = document.getElementById('standto_search_form');
+        //     if (window.location.pathname == '/archives/' || window.location.pathname == '/standto/archives/') {
 
-                //-----------------
-                //  Search
-                //--------------------------
+        //         //-----------------
+        //         //  Search
+        //         //--------------------------
 
-                //  Function that searches the on page content and displays results based off of what
-                //  the user has entered into the input.
+        //         //  Function that searches the on page content and displays results based off of what
+        //         //  the user has entered into the input.
 
-                searchForm.onkeyup = function () {
-                    var input = document.getElementById('archive-search-input'),
-                        filter = input.value.toUpperCase(),
-                        ul = document.getElementById('archive-results'),
-                        li = ul.getElementsByTagName('li'),
-                        i = 0;
+        //         searchForm.onkeyup = function () {
+        //             var input = document.getElementById('archive-search-input'),
+        //                 filter = input.value.toUpperCase(),
+        //                 ul = document.getElementById('archive-results'),
+        //                 li = ul.getElementsByTagName('li'),
+        //                 i = 0;
 
-                    for (i = 0; i < li.length; i++) {
-                        var a = li[i].getElementsByTagName('a')[0],
-                            txtValue = a.textContent || a.innerText;
+        //             for (i = 0; i < li.length; i++) {
+        //                 var a = li[i].getElementsByTagName('a')[0],
+        //                     txtValue = a.textContent || a.innerText;
 
-                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                            Helper.removeClass(li[i], 'hidden');
-                        } else {
-                            Helper.addClass(li[i], 'hidden');
-                        }
-                    }
-                    results(li);
-                };
+        //                 if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        //                     Helper.removeClass(li[i], 'hidden');
+        //                 } else {
+        //                     Helper.addClass(li[i], 'hidden');
+        //                 }
+        //             }
+        //             results(li);
+        //         };
 
-                //----------------------
-                //
-                // Displays "No Results found"
-                // 
-                //----------------------
+        //         //----------------------
+        //         //
+        //         // Displays "No Results found"
+        //         // 
+        //         //----------------------
 
-                // if the input search produces no results
-                // Alternatively, if the user has searched for something that has produced no results,
-                // then
+        //         // if the input search produces no results
+        //         // Alternatively, if the user has searched for something that has produced no results,
+        //         // then
 
-                function results(li) {
-                    var results = document.getElementById('results-text'),
-                        noResults = document.getElementById('no-results-text'),
-                        listItems = document.querySelectorAll('li.hidden');
+        //         function results(li) {
+        //             var results = document.getElementById('results-text'),
+        //                 noResults = document.getElementById('no-results-text'),
+        //                 listItems = document.querySelectorAll('li.hidden');
 
-                    if (listItems.length == li.length) {
-                        Helper.removeClass(noResults, 'hidden');
-                        Helper.addClass(results, 'hidden');
-                    } else {
-                        Helper.addClass(noResults, 'hidden');
-                        Helper.removeClass(results, 'hidden');
-                    }
-                }
+        //             if (listItems.length == li.length) {
+        //                 Helper.removeClass(noResults, 'hidden');
+        //                 Helper.addClass(results, 'hidden');
+        //             } else {
+        //                 Helper.addClass(noResults, 'hidden');
+        //                 Helper.removeClass(results, 'hidden');
+        //             }
+        //         }
 
 
-                //--------------------------
-                // GovSearch StandTo Search
-                //--------------------------
-                searchForm.onsubmit = function (e) {
-                    if (this.query.value === '') {
-                        e.preventDefault();
-                        return false;
-                    } else {
-                        this.query.value = 'Stand To ' + this.query.value;
-                        return true;
-                    }
-                };
-            }
-        })();
+        //         //--------------------------
+        //         // GovSearch StandTo Search
+        //         //--------------------------
+        //         searchForm.onsubmit = function (e) {
+        //             if (this.query.value === '') {
+        //                 e.preventDefault();
+        //                 return false;
+        //             } else {
+        //                 this.query.value = 'Stand To ' + this.query.value;
+        //                 return true;
+        //             }
+        //         };
+        //     }
+        // })();
     } else if (oldArchive){
             var archivedStandto = document.querySelectorAll('.archived-standto > p'),
                 searchText = 'Resources:',
