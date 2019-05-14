8824aebb6ae9a5ef4553c47fb79e008667902095
Sam O'Donnell <samodonnell@Christophers-iMac.local>
2019-05-14 12:11:20 -0400

    Previous push broke search feature on ardev

--- _js/standto.js
+++ _js/standto.js
@@ -267,10 +267,13 @@ var SocialBar = require('./globals/modules/SocialBar'),
 
         var searchPath = function () {
             if (pathName === '/') {
+                console.log(pathName);
                 return './';
             } else if (pathName === '/standto/') {
+                console.log(pathName);
                 return '../';
             } else {
+                console.log(pathName);
                 return '../';
             }
         }
@@ -279,7 +282,8 @@ var SocialBar = require('./globals/modules/SocialBar'),
         var sjs = new SimpleJekyllSearch({
             searchInput: document.getElementById('search-input'),
             resultsContainer: document.getElementById('results-container'),
-            json: searchPath() + 'search.json',
+            // json: searchPath() + 'search.json',
+            json: '../search.json',
             searchResultTemplate: '<li class="archive-st"><p><span class="date archive-date">{date}</span><a class="article-link" href="' + urlPath() + '{url}">{title}</a></p></li>',
             noResultsText: '<div class="no-results"><h3>No Results</h3><p>Sorry, We couldn&#39;t find anything that matches your search. Please try again</p></div>',
             limit: 35,
@@ -288,7 +292,6 @@ var SocialBar = require('./globals/modules/SocialBar'),
 
         // Calling search function
         sjs;
-
     }
 
     if (oldArchive) {
